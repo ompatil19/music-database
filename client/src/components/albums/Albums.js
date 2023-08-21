94a9571c01417b5848c4dac21604765f464fd9c2
ompatil19 <ompatil2002@gmail.com>
2023-08-21 15:13:48 +0530

    added albums backend

---
+++ client/src/components/albums/Albums.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Albums() {
+  return (
+    <>
+        <h1>Albums</h1>
+        
+    </>
+  )
+}
+
+export default Albums
--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const axios = require('axios');
+require('dotenv').config();
 // const UserModel = require("./models/Users");
 
 const cors = require("cors");
-
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+let ACCESS_TOKEN;
 app.use(express.json());
 app.use(cors());
 
@@ -12,6 +16,97 @@ mongoose.connect(
   "mongodb+srv://ompatil:chocolate19@cluster0.dsrdyr0.mongodb.net/"
 );
 
+//Spotify authorisation
+// Function to obtain the access token
+function getAccessToken() {
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    method: 'post',
+    headers: {
+      'Authorization': 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')
+    },
+    params: {
+      grant_type: 'client_credentials'
+    }
+  };
+
+  return axios(authOptions)
+    .then(response => {
+      ACCESS_TOKEN = response.data.access_token;
+      console.log('Access Token:', ACCESS_TOKEN);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+
+async function getAlbumDetails(albumName) {
+  try {
+    const searchOptions = {
+      url: 'https://api.spotify.com/v1/search',
+      method: 'get',
+      headers: {
+        'Authorization': `Bearer ${ACCESS_TOKEN}`
+      },
+      params: {
+        q: albumName,
+        type: 'album'
+      }
+    };
+
+    const response = await axios(searchOptions);
+    const albums = response.data.albums.items;
+
+    if (albums.length > 0) {
+      const album = albums[0];
+      const albumId = album.id;
+
+      const albumDetailsOptions = {
+        url: `https://api.spotify.com/v1/albums/${albumId}`,
+        method: 'get',
+        headers: {
+          'Authorization': `Bearer ${ACCESS_TOKEN}`
+        }
+      };
+
+      const albumResponse = await axios(albumDetailsOptions);
+      const detailedAlbum = albumResponse.data;
+
+      // Prepare and return the album details
+      const albumDetails = {
+        id: albumId,
+        name: detailedAlbum.name,
+        artists: detailedAlbum.artists.map(artist => artist.name),
+        release_date: detailedAlbum.release_date,
+        total_tracks: detailedAlbum.total_tracks,
+        genres: detailedAlbum.genres,
+        // Add more details as needed
+      };
+
+      return albumDetails;
+    } else {
+      throw new Error('Album not found.');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+// Example usage
+const albumName = 'sour'; // Replace with the album name you want to search for
+getAccessToken().then(()=>{getAlbumDetails(albumName)
+  .then(albumDetails => {
+    console.log('Album Details:', albumDetails);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+});
+
+
+//Route to get data from database
 app.get("/getUsers", (req, res) => {
   UserModel.find({}, (err, result) => {
     if (err) {
@@ -22,6 +117,8 @@ app.get("/getUsers", (req, res) => {
   });
 });
 
+
+//Route to add data to database
 app.post("/createUser", async (req, res) => {
   const user = req.body;
   const newUser = new UserModel(user);
@@ -29,6 +126,8 @@ app.post("/createUser", async (req, res) => {
 
   res.json(user);
 });
+
+
 
 app.listen(3001, () => {
   console.log("SERVER RUNS PERFECTLY!");
