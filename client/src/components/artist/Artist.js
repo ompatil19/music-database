430f1dfe700bb47365db5cb01402298f905c1b1a
ompatil19 <ompatil2002@gmail.com>
2023-09-04 19:43:37 +0530

    added artist and tracks

---
+++ client/src/components/artist/Artist.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios';
+function Artist() {
+  const [artistDetails, setartistDetails] = useState("");
+  const [artistname, setartistname] = useState("");
+  const changeartistname = (e) => {
+    setartistname(e.target.value);
+  }
+  let artist_details = {};
+  const getartist = (e) => {
+    axios.get(`http://localhost:3001/artist/${artistname}`).then((response) => {
+      artist_details = response.data;
+      const fetchedArtistDetails = response.data;
+      setartistDetails(fetchedArtistDetails);
+      console.log("Artist details: ", artist_details);
+    });
+  }
+  const addArtistToDB = (e) => {
+    axios.post(`http://localhost:3001/artist/${artistname}/insert`).then((response) => {
+      console.log("Artist inserted to db: ", response.data);
+    });
+  }
+  return (
+    <>
+      <div className="container d-flex text-center box justify-content-around align-items-center">
+
+        <div className='albumfetch d-flex flex-column align-items-center'>
+          {/* <MdOutlineArtTrack className='albumicon' /> */}
+          {/* <IoMdAlbums className='albumicon' /> */}
+          <h1 className='color-green'>Artist</h1>
+          <input type="text" name="title" id="title" placeholder="Album Name" onChange={changeartistname} className='albumbox' />
+
+          {/* <input type="button" value="Get album" onClick={getalbum} /> */} <br />
+          <button className="btn" onClick={getartist}>
+            <span>Get Artist</span>
+          </button>
+        </div>
+        {artistDetails && <div className='albumdet d-flex-column justify-content-center'>
+          <h1 className='color-green'>Artist Details</h1>
+          <p><span className='color-green'>ID:</span> {artistDetails.artistId}</p>
+          <p><span className='color-green'>Name: </span>{artistDetails.artistName}</p>
+          <p><span className='color-green'>Follower</span> {artistDetails.followers}</p>
+          <p><span className='color-green'>Genres</span> {artistDetails.genres}</p>
+          <button className="btn" onClick={addArtistToDB}>
+            <span>Add Artist</span>
+          </button>
+        </div>}
+      </div>
+    </>
+  )
+}
+
+export default Artist
---
+++ client/src/components/tracks/Tracks.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useState } from 'react'
+function Tracks() {
+    // const [trackDetails, settrackDetails] = useState("");
+    // let track_details = {};
+    // const gettrack = (e) => {
+    //   axios.get(`http://localhost:3001/album/${albumname}`).then((response) => {
+    //     track_details = response.data;
+    //     const fetchedtrackDetails = response.data;
+    //     settrackDetails(fetchedtrackDetails);
+    //     console.log("Track details: ", track_details);
+    //   });
+    //   // console.log("Album details: ", album_details);
+    // }
+    // const addtracktodb = (e) => {
+    //   axios.post(`http://localhost:3001/album/${albumname}/insert`).then((response) => {
+    //     console.log("Track inserted to db: ", response.data);
+    //   });
+    // }
+  return (
+    <>
+    </>
+//     <>
+//     <div className="container d-flex text-center box justify-content-around align-items-center">
+
+//       <div className='albumfetch d-flex flex-column align-items-center'>
+//         <MdOutlineArtTrack className='albumicon' />
+//         {/* <IoMdAlbums className='albumicon' /> */}
+//         <h1 className='color-green'>Albums</h1>
+//         <input type="text" name="title" id="title" placeholder="Album Name" onChange={changealbumname} className='albumbox' />
+
+//         {/* <input type="button" value="Get album" onClick={getalbum} /> */} <br />
+//         <button className="btn" onClick={getalbum}>
+//           <span>Get Track</span>
+//         </button>
+//       </div>
+//       {albumDetails && <div className='albumdet d-flex-column justify-content-center'>
+//         <h1 className='color-green'>Track Details</h1>
+//         <p><span className='color-green'>ID:</span> {albumDetails.albumId}</p>
+//         <p><span className='color-green'>Name: </span>{albumDetails.albumName}</p>
+//         <p><span className='color-green'>Artists:</span> {albumDetails.artists}</p>
+//         <p><span className='color-green'>Release Date:</span> {albumDetails.releaseDate}</p>
+//         <p><span className='color-green'>Total Tracks:</span> {albumDetails.total_tracks}</p>
+//         <button className="btn" onClick={addalbumtodb}>
+//           <span>Add Album</span>
+//         </button>
+//       </div>}
+//     </div>
+//   </>
+  )
+}
+
+export default Tracks
