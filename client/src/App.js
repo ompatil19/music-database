96b68627e5e55c60a4971060005327444fe49db7
ompatil19 <ompatil2002@gmail.com>
2023-09-04 19:43:17 +0530

    chnaged navbar added artist and tracks

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import Albums from './components/albums/Albums';
 import Navbar from './components/navbar/Navbar';
 import Landing from './components/landing/Landing';
+import Tracks from './components/tracks/Tracks';
+import Artist from './components/artist/Artist';
 function App() {
   return (
     <>
     <Navbar/>
-    <Albums/>
+    {/* <Albums/> */}
     {/* <Landing/> */}
+    {/* <Tracks/> */}
+    <Artist/>
+
     </>
   );
 }
--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <>
     <nav className="navbar navbar-expand-lg custom-nav">
   <div className="container-fluid">
-    <a className="navbar-brand ms-5" href="#">Chordistry</a>
+    <a className="navbar-brand" href="#">Chordistry</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
